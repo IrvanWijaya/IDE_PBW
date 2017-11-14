cae49fc49cd5c4d3a1c4bacc733949b285489f94
Irvan Wijaya <irvanwijayaludianto@gmail.com>
2017-11-14 23:22:09 +0700

    header done

---
+++ layout/header.php
@@ -0,0 +1,13 @@
+<div class="w3-container header">
+    <div class="w3-right tempImg">
+        <img src="../../img/logo.png">
+    </div>
+
+    <div class= "w3-rest">
+        <h1>Interactive Digital Learning Environment</h1>
+        <p class= "w3-medium">Faculty of Information Technology and Science</p>
+        <p class = "w3-medium">Parahyangan Catholic University</p>
+        
+        <hr>
+    </div>
+</div>
--- pages/student/std.php
+++ pages/student/std.php
@@ -1,15 +1,26 @@
 <!-- include connection -->
+<?php 
+	include("../../phpScript/connection.php");
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>IDE</title>
-		<!-- include style -->
+		<?php 
+			include("../../layout/style.php");
+		?>
 	</head>
 	
 	<body>
 		<?php $myCourses = false ?>
 		<!-- include header -->
+		<?php 
+			include("../../layout/header.php");
+		?>
 		<div class="w3-main">
+			<?php 
+				include("../../layout/sidebar.php");
+			?>
 			<!-- include sidebar -->
 		</div>
 	</body>
