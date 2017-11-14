99b0caa47006510802795f1081665835a4c6b589
Irvan Wijaya <irvanwijayaludianto@gmail.com>
2017-11-14 19:33:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>IDE</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="lib/w3.css">
+		<link rel="stylesheet" href="lib/w3-theme-dark-grey.css">
+		<link rel="stylesheet" href="style/style.css">
+		<link rel="stylesheet" href="lib/font-awesome.min.css">
+		<link rel="stylesheet" href="lib/font-awesome.css">
+	</head>
+	<body style="color: white">
+		<div id="bgImage"></div>
+		<div class= "w3-row">
+			<div class="w3-container w3-threequarter"></div>
+			<div class="w3-container w3-quarter">
+				<button class="w3-btn w3-light-gray w3-right w3-opacity-min btnMenuIndex">Help</button>
+				<button class="w3-btn w3-light-gray w3-right w3-opacity-min btnMenuIndex">Contact us</button>
+				<button class="w3-btn w3-light-gray w3-right w3-opacity-min btnMenuIndex">About us</button>
+			</div>
+
+			<div class="w3-third w3-display-left">
+				<div class="w3-container ">
+					<h1 class="w3-xxxlarge w3-text-white">IDE</h1>
+					<p class = "w3-large w3-text-white">Interactive Digital Learning Environment</p>
+					<p class= "w3-text-white w3-small">-Faculty of Information Technology and Science-</p>
+					<button id="btnLogin" class="w3-btn w3-light-gray w3-opacity-min">Login</button>
+				</div>
+			</div>
+
+			<div class="w3-display-bottomleft" style = "margin-left:20px">
+				<p>&copy; Developed by Irvan & Nico</p>
+			</div>
+			
+		</div>		
+	</body>
+</html>
