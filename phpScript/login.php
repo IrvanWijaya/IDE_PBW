78d0672161b75e07a9bdb20154aede2e777ddd1b
NICO SAMUEL <i15007@ftis.unpar>
2017-11-15 12:12:39 +0700

    ubah login.php kya modul baru 15/11/2017 12:12

--- phpScript/login.php
+++ phpScript/login.php
@@ -1,7 +1,6 @@
 <?php
     include 'connection.php';
-    $userID = "a";
-    $name = "b";
+    include 'startSession.php';
 
     if (isset($_POST['username']) && isset($_POST['password'])) {
         $username = $_POST['username'];
@@ -16,16 +15,22 @@
             } else {
                 if ($row['pass'] == $password) {
 
-                    $userID = $row['userID'];
-                    $name = $row['name'];
-                    echo "$row[userID]";
+                    /*$query = "SELECT userID, users.name, usergroups.name as role FROM usergroups JOIN users ON usergroups.ID_UG = users.ID_UG
+                                WHERE username = $username";*/
 
-                    $query = "SELECT userID, users.name, usergroups.name as role FROM usergroups JOIN users ON usergroups.ID_UG = users.ID_UG
-                                WHERE username = $username";
+                    $query = "SELECT users.ID_U as id, users.username as username,
+                            users.pass as pass, users.userID as userid,
+                            users.name as name, usergroups.name as position
+                            FROM users join usergroups
+                            ON users.ID_UG = usergroups.ID_UG
+                            WHERE users.username = $username";
+
                     $result = $conn->query($query);
                     $row = $result->fetch_array();
+
+                    $_SESSION['username'] = $row['username'];
                     
-                    if ($row['role'] == "lecturer") {
+                    if ($row['position'] == "lecturer") {
                         header('Location: ../pages/lecturer/lct.php');
                     } else {
                         header('Location: ../pages/student/std.php');
